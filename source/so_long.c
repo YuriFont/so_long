bfe99324de48f7fd6e232746da933f9415bf1330
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-02-24 17:49:58 -0300

    Verification of Open function

--- source/so_long.c
+++ source/so_long.c
@@ -6,14 +6,14 @@
 /*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 09:10:31 by yufonten          #+#    #+#             */
-/*   Updated: 2024/02/24 14:59:29 by yufonten         ###   ########.fr       */
+/*   Updated: 2024/02/24 16:07:43 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include <stdio.h>
 
-void	initializing_window(t_data *data)
+void	initializing_window(t_game *data)
 {
 	data->ptr_mlx = mlx_init();
 	if (!data->ptr_mlx)
@@ -26,7 +26,7 @@ void	initializing_window(t_data *data)
 	}
 }
 
-int	destroy_window(t_data *data)
+int	destroy_window(t_game *data)
 {
 	mlx_destroy_window(data->ptr_mlx, data->w_mlx);
 	mlx_destroy_display(data->ptr_mlx);
@@ -35,7 +35,7 @@ int	destroy_window(t_data *data)
 	return (0);
 }
 
-int	on_keypress(int key, t_data *data)
+int	on_keypress(int key, t_game *data)
 {
 	if (key == 65307)
 		destroy_window(data);
@@ -45,25 +45,27 @@ int	on_keypress(int key, t_data *data)
 
 int	main(int ac, char **av)
 {
-	t_data	data;
-
 	if (ac == 2)
 	{
-		if (!check_arq(av[1]))
+		t_game	data;
+		if (!check_arq(av[1]) || !(data.fd = open(av[1], O_RDONLY)))
 		{
-			ft_printf("error\n");
+			ft_printf("Esse arquivo não existe ou não é .ber\n");
 			exit(1);
 		}
 		initializing_window(&data);
 		data.image_gram = mlx_xpm_file_to_image(data.ptr_mlx, GRAM, &data.lenght, &data.lenght);
 		data.image_tree = mlx_xpm_file_to_image(data.ptr_mlx, TREE, &data.lenght, &data.lenght);
-		mlx_put_image_to_window(data.ptr_mlx, data.w_mlx, data.image_gram, 50, 50);
+		data.image_kidf = mlx_xpm_file_to_image(data.ptr_mlx, KID_F, &data.lenght, &data.lenght);
+		mlx_put_image_to_window(data.ptr_mlx, data.w_mlx, data.image_kidf, 50, 50);
 		mlx_put_image_to_window(data.ptr_mlx, data.w_mlx, data.image_tree, 0, 0);
+		mlx_put_image_to_window(data.ptr_mlx, data.w_mlx, data.image_tree, 0, 50);
 		mlx_key_hook(data.w_mlx, on_keypress, &data);
 		mlx_hook(data.w_mlx, 17, 0, destroy_window, &data);
 		mlx_loop(data.ptr_mlx);
 	}
 	else
+		ft_printf("Faltam argumentos");
 		
 	
 	return (0);
--- source/so_long.h
+++ source/so_long.h
@@ -6,7 +6,7 @@
 /*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 09:11:12 by yufonten          #+#    #+#             */
-/*   Updated: 2024/02/24 15:00:59 by yufonten         ###   ########.fr       */
+/*   Updated: 2024/02/24 16:00:24 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,23 @@
 # include "../libraries/libft/libft.h"
 # include "../libraries/ft_printf/ft_printf.h"
 # include <stdlib.h>
+# include <unistd.h>
+#include <fcntl.h>
 
 # define GRAM "./assets/gram.xpm"
 # define TREE "./assets/tree.xpm"
+# define KID_F "./assets/ash_front.xpm"
 
-typedef struct s_data
+typedef struct s_game
 {
 	void	*ptr_mlx;
 	void	*w_mlx;
 	void	*image_gram;
 	void	*image_tree;
+	void	*image_kidf;
+	int		fd;
 	int		lenght;
-}	t_data;
+}	t_game;
 
 /* init_map.c */
 int	check_arq(char *arq);
